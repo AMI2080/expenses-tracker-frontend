adf813bd81628721a05232bedf97c113ea083154
AMI2080 <AMI_2080@yahoo.com>
2025-12-23 08:36:54 +0200

    remove empty scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthService, type UserDetails } from './auth/auth.service';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   private readonly authService = inject(AuthService);
