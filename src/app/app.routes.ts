d358b1dd5cf3f6ff2c01cacdb1ed8e7053e9105a
AMI2080 <AMI_2080@yahoo.com>
2025-12-23 08:30:29 +0200

    Public Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { guestGuard } from './guards/guest.guard';
-import { authGuard } from './guards/auth.guard';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -8,8 +7,7 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    canActivate: [authGuard]
+    component: HomeComponent
   },
   {
     path: 'auth',
